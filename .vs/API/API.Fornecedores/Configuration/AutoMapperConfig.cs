dc5fb2f200b193cf9418443b6f15b466b947007e
pauloviniciussud <pauloviniciussud@gmail.com>
2020-08-02 11:19:10 -0300

    Construindo o controller de fornecedores

---
+++ .vs/API/API.Fornecedores/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using API.Fornecedores.DTO;
+using AutoMapper;
+using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Fornecedores.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorDTO>().ReverseMap();
+            CreateMap<Endereco, EnderecoDTO>().ReverseMap();
+            CreateMap<Produto, ProdutoDTO>().ReverseMap();
+        }
+    }
+}

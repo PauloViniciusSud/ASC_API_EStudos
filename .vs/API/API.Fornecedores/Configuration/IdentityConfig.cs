d089ee7bd32264eb41d97651c9f408525be1424a
pauloviniciussud <pauloviniciussud@gmail.com>
2020-08-18 08:51:17 -0300

    Configuracao identity

---
+++ .vs/API/API.Fornecedores/Configuration/IdentityConfig.cs
@@ -0,0 +1,32 @@
+﻿using API.Fornecedores.Date;
+using DevIO.Data.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Fornecedores.Configuration
+{
+    public static class IdentityConfig
+    {
+
+        public static IServiceCollection AddIdentityConfiguration
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddDbContext<AplicationDbContext>(options => 
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<IdentityUser, IRouteCollection>().AddEntityFrameworkStores<AplicationDbContext>()
+              .AddDefaultTokenProviders();
+
+
+            return services;
+        }
+    }
+}
